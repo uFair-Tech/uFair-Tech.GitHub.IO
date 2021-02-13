eca6e6374b798ee02787296b90807af1f72451a0
uFair-Tech <uFair-UNNC@outlook.com>
2021-02-14 00:45:04 +0800

    Feb 2021

---
+++ precache-manifest.34b7bd216f8fbfc8ccbe01f1763a6f76.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01ea6ea1bebd79743e15f957606791c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0156059fa8d6759472d4",
+    "url": "./static/css/2.14899007.chunk.css"
+  },
+  {
+    "revision": "14e45c09dd515c2e8c95",
+    "url": "./static/css/main.90d978e8.chunk.css"
+  },
+  {
+    "revision": "0156059fa8d6759472d4",
+    "url": "./static/js/2.b8d647a0.chunk.js"
+  },
+  {
+    "revision": "ccf491244da6a3591a7f18b2d9dc8874",
+    "url": "./static/js/2.b8d647a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e45c09dd515c2e8c95",
+    "url": "./static/js/main.80462c2b.chunk.js"
+  },
+  {
+    "revision": "295dd27d1228306ab2e1",
+    "url": "./static/js/runtime-main.c9769c5a.js"
+  },
+  {
+    "revision": "bc24eb5ae39d9452fcb3dad5b00f7b95",
+    "url": "./static/media/Chalkduster.bc24eb5a.woff"
+  },
+  {
+    "revision": "0d55d72d7bdb46043b736daa706149d2",
+    "url": "./static/media/StudentsIMG.0d55d72d.jpg"
+  },
+  {
+    "revision": "79060aee3893fb78fce5f4e0b602e812",
+    "url": "./static/media/StudentsIMGs.79060aee.jpg"
+  },
+  {
+    "revision": "37cff9d3d53722aba1f120c69798d7e0",
+    "url": "./static/media/arrow.37cff9d3.svg"
+  },
+  {
+    "revision": "aa91db727936b1d3cb6e29dcce873c9e",
+    "url": "./static/media/background.aa91db72.png"
+  },
+  {
+    "revision": "af50d6c3d095a031531d3419c62525a2",
+    "url": "./static/media/background.af50d6c3.png"
+  },
+  {
+    "revision": "eac21ac8d0e62298f7cfb8558acb2aeb",
+    "url": "./static/media/background.eac21ac8.jpg"
+  },
+  {
+    "revision": "c5ee37c6801e661e6068f7c79e1b5d7c",
+    "url": "./static/media/image.c5ee37c6.png"
+  },
+  {
+    "revision": "f7ff0158b44acf49f41f26bb752a6b18",
+    "url": "./static/media/ucourse-code.f7ff0158.jpg"
+  },
+  {
+    "revision": "85744b5c567fcae12000e636324c26ce",
+    "url": "./static/media/ucourse.85744b5c.png"
+  },
+  {
+    "revision": "acb9c907542eec03d8aba94c99d4d6da",
+    "url": "./static/media/ufair-logo-crop.acb9c907.png"
+  },
+  {
+    "revision": "16d425d9f087236015f5298b3370adc3",
+    "url": "./static/media/ufair-logo.16d425d9.png"
+  },
+  {
+    "revision": "cfdb70c3587769327f760ca0a319bd46",
+    "url": "./static/media/umap-code.cfdb70c3.png"
+  },
+  {
+    "revision": "5890c37ae99fcfec4f9623bccaeb9b65",
+    "url": "./static/media/umap.5890c37a.png"
+  }
+]);
